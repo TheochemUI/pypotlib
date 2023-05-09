8c44b473e1ed0f5a2c2b8cff48764b8567066ef6
Rohit Goswami <rog32@hi.is>
2023-05-09 17:51:20 +0000

    ENH: Add deepcopy and pickle to CuH2
    This is a trivial implementation

--- pyb11_srcs/CuH2/py_cuh2pot.cc
+++ pyb11_srcs/CuH2/py_cuh2pot.cc
@@ -4,5 +4,18 @@ using namespace rgpot;
 void py_cuh2pot(py::module &m) {
     py::class_<CuH2Pot, Potential, PyCuH2Pot<>>(m, "CuH2Pot")
         .def(py::init<>())
-        .def("__call__", &CuH2Pot::operator());
+        .def("__call__", &CuH2Pot::operator())
+        .def("__copy__", [](const CuH2Pot &self){
+            return CuH2Pot(self);
+        })
+        // XXX: These don't really do anything, currently just compatibility stuff
+        .def("__deepcopy__", [](const CuH2Pot &self, py::dict){
+            return CuH2Pot(self);
+        }, "memo"_a)
+        .def(py::pickle([](const CuH2Pot &p){
+                return py::make_tuple(p);
+        }, [](py::tuple t){
+                CuH2Pot p(t[0].cast<CuH2Pot>());
+                return p;
+        }));
 };
